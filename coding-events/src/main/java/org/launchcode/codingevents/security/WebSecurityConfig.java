d08e031263f54cfba60ca2468a1a57e64e17ee51
Moldo👻 Alex <alexandrum255@gmail.com>
2024-03-27 21:39:34 +0200

    Fixed thymeleaf pathing that made postmapping give Forbidden Error

--- coding-events/src/main/java/org/launchcode/codingevents/security/WebSecurityConfig.java
+++ coding-events/src/main/java/org/launchcode/codingevents/security/WebSecurityConfig.java
@@ -2,16 +2,13 @@ package org.launchcode.codingevents.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,9 +21,9 @@ public class WebSecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/", "/home").permitAll() // Allow access to these URLs to all users
-                        .requestMatchers(HttpMethod.GET,"/events/create", "/events/delete", "/events/update").hasAnyRole("ADMIN", "USER")
-                        .requestMatchers(HttpMethod.POST,"/events/create", "/events/delete", "/events/update").hasAnyRole("ADMIN", "USER")
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/events/create", "/events/delete").hasRole("ADMIN")
+                        .requestMatchers("/events", "/events").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
@@ -46,25 +43,5 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        String encodedPassword = passwordEncoder().encode("user"); // Encode password
-
-        UserDetails user = User.builder()
-                .username("user")
-                .password(encodedPassword) // Use encoded password
-                .roles("USER")
-                .build();
-
-        encodedPassword = passwordEncoder().encode("admin"); // Encode password
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(encodedPassword) // Use encoded password
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
---
+++ coding-events/src/main/java/org/launchcode/codingevents/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package org.launchcode.codingevents.service;
+
+
+import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName().name()))
+                        .collect(Collectors.toList()));
+    }
+}
